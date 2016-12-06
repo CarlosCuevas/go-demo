355fc7e22813813efa185d022abd2b0eae9728f1
Carlos Cuevas <carloscuevas1@gmail.com>
2016-12-06 00:01:02 -0500

    working endpoint

--- src/app/main.go
+++ src/app/main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
 )
 
-func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
-	//params := mux.Vars(req)
-	m1 := Matchup{{Name: "Giants", Score: 123}, {Name: "Patriots", Score: 100}}
-	m2 := Matchup{{Name: "Jets", Score: 53}, {Name: "Cardinals", Score: 90}}
-	m3 := Matchup{{Name: "Jags", Score: 83}, {Name: "Broncos", Score: 38}}
-	m4 := Matchup{{Name: "Bills", Score: 73}, {Name: "Browns", Score: 104}}
-	m5 := Matchup{{Name: "Chargers", Score: 84}, {Name: "Falcons", Score: 134}}
-	matchups := []Matchup{m1, m2, m3, m4, m5}
-	teams := QuantifyLuck(matchups)
-	json.NewEncoder(w).Encode(teams)
+func PostLuckEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	// m1 := Matchup{{Name: "Giants", Score: 123}, {Name: "Patriots", Score: 100}}
+	// m2 := Matchup{{Name: "Jets", Score: 53}, {Name: "Cardinals", Score: 90}}
+	// m3 := Matchup{{Name: "Jags", Score: 83}, {Name: "Broncos", Score: 38}}
+	// m4 := Matchup{{Name: "Bills", Score: 73}, {Name: "Browns", Score: 104}}
+	// m5 := Matchup{{Name: "Chargers", Score: 84}, {Name: "Falcons", Score: 134}}
+	// matchups := []Matchup{m1, m2, m3, m4, m5}
+
+	dec := json.NewDecoder(req.Body)
+	for {
+		var m Matchups
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(QuantifyLuck(m))
+	}
+
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/luck", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/luck", PostLuckEndpoint).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- src/app/types.go
+++ src/app/types.go
@@ -2,26 +2,28 @@ package main
 
 // Team
 type Team struct {
-    Name string   `json:"name,omitempty"`
-    Score  int   `json:"score,omitempty"`
-    Result bool `json:"result,omitempty"`
-    Odds float64 `json:"odds,omitempty"`
-    Luck float64 `json:"luck,omitempty"`
+	Name   string  `json:"name,omitempty"`
+	Score  int     `json:"score,omitempty"`
+	Result bool    `json:"result,omitempty"`
+	Odds   float64 `json:"odds,omitempty"`
+	Luck   float64 `json:"luck,omitempty"`
 }
 
 type Teams []Team
 
 func (slice Teams) Len() int {
-    return len(slice)
+	return len(slice)
 }
 
 func (slice Teams) Less(i, j int) bool {
-    return slice[i].Score < slice[j].Score;
+	return slice[i].Score < slice[j].Score
 }
 
 func (slice Teams) Swap(i, j int) {
-    slice[i], slice[j] = slice[j], slice[i]
+	slice[i], slice[j] = slice[j], slice[i]
 }
 
 // Matchup
 type Matchup [2]Team
+
+type Matchups []Matchup
