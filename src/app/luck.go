4337fc85229e57e37c6dc28908ff453f82e01411
Carlos Cuevas <carloscuevas1@gmail.com>
2016-12-08 21:38:56 -0500

    added comments

--- src/app/luck.go
+++ src/app/luck.go
@@ -5,6 +5,7 @@ import "sort"
 func QuantifyLuck(matchups []Matchup) Teams {
 	teams := Teams{}
 
+    // extract teams from matchups
 	for _, m := range matchups {
 		m[0].Result = m[0].Score > m[1].Score
 		m[1].Result = !m[0].Result
@@ -12,10 +13,15 @@ func QuantifyLuck(matchups []Matchup) Teams {
 		teams = append(teams, m[1])
 	}
 
+    // sort teams by score
 	sort.Sort(teams)
 
 	for i, t := range teams {
+        // index / number of other teams tells us how many matchups you would've won
 		teams[i].Odds = float64(i) / float64(len(teams)-1)
+        // taking the difference of your actual result (win = 1, loss = 0) and
+        // your odds of winning tells us how lucky/unlucky you are (higher the
+        // positive number is lucky, lower the negative number is unlucky)
 		teams[i].Luck = float64(btoi(t.Result)) - teams[i].Odds
 	}
 
